db80e7c2a32eb772d00ec23a3ebeda6871dca04b
netsharamalisha <nethsaramalisha99@gmail.com>
2025-04-15 19:08:45 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "session id",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=A0oC1TZI#QFifl_hqU6HTyjDca8DQDr1uBtNQKhae9pCjrv5EsoQ",
 PREFIX: process.env.PREFIX || '.' ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
